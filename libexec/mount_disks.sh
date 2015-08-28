4ff5745c44c5cfe66953ed16513efbc2b908a496
Patrick Clay <pclay@google.com>
2015-08-28 14:41:06 -0700

    Slightly less implementation of RAID 0 over local SSDs

--- libexec/mount_disks.sh
+++ libexec/mount_disks.sh
@@ -20,66 +20,25 @@ set -e
 DEBIAN_FRONTEND=noninteractive install_application "mdadm"
 
 # Create the software RAID volume on all four Local SSD drives
-mdadm --create --verbose /dev/md0 --level=stripe --raid-devices=4 /dev/sdb /dev/sdc /dev/sdd /dev/sde
-
-# Get a list of disks from the metadata server.
-BASE_DISK_URL='http://metadata.google.internal/computeMetadata/v1beta1/instance/disks/'
-DISK_PATHS=$(curl ${BASE_DISK_URL})
-MOUNTED_DISKS=()
+DISK_ID="/dev/md0"
+mdadm --create --verbose ${DISK_ID} --level=stripe --raid-devices=4 /dev/sdb /dev/sdc /dev/sdd /dev/sde
 
 # Mount the software RAID volume
-DISK_ID="/dev/md0"
 DATAMOUNT="/mnt/md0"
 mkdir -p ${DATAMOUNT}
-MOUNTED_DISKS+=(${DATAMOUNT})
 echo "Mounting '${DISK_ID}' under mount point '${DATAMOUNT}'..."
 MOUNT_TOOL=/usr/share/google/safe_format_and_mount
 ${MOUNT_TOOL} -m 'mkfs.ext4 -F' ${DISK_ID} ${DATAMOUNT}
 
-for DISK_PATH in ${DISK_PATHS}; do
-  # Use the metadata server to determine the official index/name of each disk.
-  DISK_NAME=$(curl ${BASE_DISK_URL}${DISK_PATH}device-name)
-  DISK_INDEX=$(curl ${BASE_DISK_URL}${DISK_PATH}index)
-  DISK_TYPE=$(curl ${BASE_DISK_URL}${DISK_PATH}type)
-
-  # Index '0' is the boot disk and is thus already mounted.
-  if [[ "${DISK_INDEX}" == '0' ]]; then
-    echo "Boot disk is ${DISK_NAME}; will not attempt to mount it."
-    continue
-  fi
-
-  if [[ "${DISK_TYPE}" == 'EPHEMERAL' ]]; then
-    echo "Skipping Ephemeral Disk $(DISK_NAME) assuming it is part of the software RAID volume."
-    continue
-    DISK_PREFIX='ed'
-  elif [[ "${DISK_TYPE}" == 'PERSISTENT' ]]; then
-    DISK_PREFIX='pd'
-  fi
-
-  # The metadata-specified 'name' can be converted to a disk 'id' by prepending
-  # 'google-' and finding it under /dev/disk/by-id.
-  DISK_ID="/dev/disk/by-id/google-${DISK_NAME}"
-  echo "Resolved disk name '${DISK_NAME}' to expected path '${DISK_ID}'."
-
-  # We will name the mount-point after the official 'disk index'; this means
-  # there will be no mounted disk with suffix '0' since '0' is the boot disk.
-  DATAMOUNT="/mnt/${DISK_PREFIX}${DISK_INDEX}"
-  mkdir -p ${DATAMOUNT}
-  MOUNTED_DISKS+=(${DATAMOUNT})
-  echo "Mounting '${DISK_ID}' under mount point '${DATAMOUNT}'..."
-  MOUNT_TOOL=/usr/share/google/safe_format_and_mount
-  ${MOUNT_TOOL} -m 'mkfs.ext4 -F' ${DISK_ID} ${DATAMOUNT}
-
-  # Idempotently update /etc/fstab
-  if cut -d '#' -f 1 /etc/fstab | grep -qvw ${DATAMOUNT}; then
-    DISK_UUID=$(blkid ${DISK_ID} -s UUID -o value)
-    MOUNT_ENTRY=($(grep -w ${DATAMOUNT} /proc/mounts))
-    # Taken from /usr/share/google/safe_format_and_mount
-    MOUNT_OPTIONS='defaults,discard'
-    echo "UUID=${DISK_UUID} ${MOUNT_ENTRY[@]:1:2} ${MOUNT_OPTIONS} 0 2 \
-        # added by bdutil" >> /etc/fstab
-  fi
-done
+# Idempotently update /etc/fstab
+if cut -d '#' -f 1 /etc/fstab | grep -qvw ${DATAMOUNT}; then
+  DISK_UUID=$(blkid ${DISK_ID} -s UUID -o value)
+  MOUNT_ENTRY=($(grep -w ${DATAMOUNT} /proc/mounts))
+  # Taken from /usr/share/google/safe_format_and_mount
+  MOUNT_OPTIONS='defaults,discard'
+  echo "UUID=${DISK_UUID} ${MOUNT_ENTRY[@]:1:2} ${MOUNT_OPTIONS} 0 2 \
+      # added by bdutil" >> /etc/fstab
+fi
 
 # If disks are mounted use the first one to hold target of symlink /hadoop
 if (( ${#MOUNTED_DISKS[@]} )); then
